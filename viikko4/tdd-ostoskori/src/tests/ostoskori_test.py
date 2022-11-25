dccf37cfd245450588902c8b72e683b6fa40d414
Jerry <tammi.jerry@gmail.com>
2022-11-25 09:41:18 +0200

    Kahden eri tuotteen lisäämisen jälkeen ostoskorissa on 2 tavaraa

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -5,6 +5,8 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
+        self.maito = Tuote("Maito", 3)
+        self.mehu = Tuote("Mehu", 3)
 
     # step 1
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
@@ -14,14 +16,19 @@ class TestOstoskori(unittest.TestCase):
 
     # step 2
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_tavara(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 1)
 
     # step 3
     def test_yhden_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_sama_kuin_tuotteen_hinta(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
 
-        self.assertEqual(self.kori.hinta(), maito.hinta())
+        self.assertEqual(self.kori.hinta(), self.maito.hinta())
+
+    # step 4
+    def test_kahden_tuotteen_lisaamisen_jalkeen_ostoskorissa_kaksi_tuotetta(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.mehu)
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
